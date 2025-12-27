7d5f159ff1f09ac0e0f63fd12bfd088e94274edc
Mikhail Morozov <morozov.mi@phystech.edu>
2025-12-27 23:55:46 +0300

    Update surrogate_model.cpp

--- cpp/surrogate_model.cpp
+++ cpp/surrogate_model.cpp
@@ -4,18 +4,18 @@
 static const int NUM_COEFFS = 9;
 
 static const double COEFFS[NUM_COEFFS] = {
-    0.0,
-    0.0,
-    0.0,
-    0.0,
-    0.0,
-    0.0,
-    0.0,
-    0.0,
-    0.0
+    1.9999999999999778e+00,
+    1.0000000000000002e-01,
+    -3.0000000000001498e-01,
+    1.0000000000001501e-02,
+    -2.0000000000000236e-02,
+    6.5768450064057837e-15,
+    -4.9744241963811559e-16,
+    6.6146802458660912e-16,
+    -3.2119830520479829e-17
 };
 
-static const double INTERCEPT = 0.0;
+static const double INTERCEPT = 1.0000000000000025e+01;
 
 double kmax_model(double mach, double alpha)
 {
